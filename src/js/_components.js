fa46525495dcf6683c88f103af5bbfc0b7f58e67
BAO-1987 <andriy.bezkorovajnyj@gmail.com>
2024-04-19 13:31:12 +0300

    add product

--- src/js/_components.js
+++ src/js/_components.js
@@ -1,4 +1,5 @@
 import './components/_burger';
 import './components/_filter';
+import './components/_filterPortfolio';
 import './components/_slider';
 import './components/_sliderThumbs';
--- src/js/components/_filterPortfolio.js
+++ src/js/components/_filterPortfolio.js
@@ -1,12 +1,21 @@
 const filterList = document?.querySelector(".filter__nav");
 const filterButtons = filterList?.querySelectorAll(".btn--filter");
-const conferences = document?.querySelectorAll(".filter__content");
+const conferences = document?.querySelectorAll(".filter__content--portfolio");
+const conferencesAll = document?.querySelector(".filter__content"); // 
+const isPortfolioPage = conferences.length > 0;
+
 let conferenceIndex = 0;
 
-conferences?.forEach((conference) => {
+conferences ?.forEach((conference) => {
   conference.style.viewTransitionName = `conf-${++conferenceIndex}`;
 });
 
+if (isPortfolioPage) {
+  filterEvents("cafe");
+  updateActiveButton(filterList.querySelector('[data-filter="cafe"]'));
+} else if (conferencesAll) { 
+  conferencesAll.removeAttribute("hidden"); 
+}
 
 filterButtons?.forEach((button) => {
   button.addEventListener("click", (e) => {
@@ -33,7 +42,6 @@ function filterEvents(filter) {
   conferences.forEach((conference) => {
     let eventCategory = conference.getAttribute("data-category");
 
-    
     if (filter === "all" || filter === eventCategory) {
       conference.removeAttribute("hidden");
     } else {
